bc6dfb850a52ad1f21fe7351bd6ea9a631e360cb
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-02-20 08:16:01 +0600

    finds the perimeter of a rectangle

---
+++ Day-1-10/Day-01/problem-04.js
@@ -0,0 +1,6 @@
+// Problem-04:: Create a function that takes length and width and finds the perimeter of a rectangle.
+
+function rectangle(length,width){
+    return length*2 + width*2 
+}
+console.log(rectangle(20,10))
